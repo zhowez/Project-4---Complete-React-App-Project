cea44e8f4fcdb77d680e91f1b97562d28d9de5fe
Zach Howes <zhowes14@icloud.com>
2022-06-22 01:54:28 -0400

    set up array to hold the user list and the component to display the list. DID NOT USE RANDOM ID AS IN VIDEO BECAUSE THAT IS NOT SAFE

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from "react";
+import { useState } from "react";
 import AddUser from "./Components/Users/AddUser";
+import UsersList from "./Components/Users/UsersList";
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+  const [nextID, setNextID] = useState(0);
+
+  const nextIDHandler = () => {
+    setNextID(nextID + 1);
+  };
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      //would prefer to make a user object class then create and pass that object to the list
+      return [...prevUsersList, { name: uName, age: uAge, id: nextID }];
+    });
+    nextIDHandler();
+  };
+
   return (
     <div>
-      <AddUser />
+      <AddUser onAddUser={addUserHandler} />
+
+      <UsersList users={usersList} />
     </div>
   );
 }
